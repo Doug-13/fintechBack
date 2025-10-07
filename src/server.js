75537031144b68b46974a116575a84a2f81307e7
Douglas Mello <87047259+Doug-13@users.noreply.github.com>
2025-10-07 07:36:19 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,64 +1,4 @@
-<<<<<<< HEAD
 // src/server.js
-import mongoose from "mongoose";
-import { env } from "./config/env.js";
-import { createApp } from "./app.js";
-
-const app = createApp();
-
-mongoose
-  .connect(env.mongoUri)
-  .then(() => {
-    app.listen(env.port, () =>
-      console.log(`🚀 API on http://localhost:${env.port}`)
-    );
-  })
-  .catch((err) => {
-    console.error("❌ Mongo connection error:", err);
-=======
-// // src/server.js
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-// import morgan from 'morgan';
-// import { env } from './config/env.js';
-
-// import authRoutes from './routes/auth.js';
-// import userRoutes from './routes/users.js';
-// import eventsRoutes from './routes/events.js'; // 👈 importe as rotas de eventos (com .js)
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan(env.nodeEnv === 'development' ? 'dev' : 'combined'));
-
-// // Endpoints
-// app.use('/auth', authRoutes);
-// app.use('/users', userRoutes);
-// app.use('/eventos', eventsRoutes);             // 👈 monta aqui: agora POST /eventos existe
-
-// // Healthcheck para teste rápido
-// app.get('/health', (req, res) => res.json({ ok: true, time: new Date().toISOString() }));
-
-// // 404 amigável (coloque DEPOIS das rotas)
-// app.use((req, res) => {
-//   console.warn('[404]', req.method, req.originalUrl);
-//   res.status(404).json({ error: 'Not found', path: req.originalUrl });
-// });
-
-// // Handler de erro (por último)
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).json({ error: 'Erro interno' });
-// });
-
-// mongoose.connect(env.mongoUri).then(() => {
-//   app.listen(env.port, () => console.log(`API on http://localhost:${env.port}`));
-// }).catch(err => {
-//   console.error('Mongo error:', err);
-//   process.exit(1);
-// });
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -67,7 +7,7 @@ import { env } from './config/env.js';
 
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
-import eventsRoutes from './routes/events.js'; // 👈 Rotas de eventos
+import eventsRoutes from './routes/events.js'; // Rotas de eventos
 
 const app = express();
 
@@ -105,6 +45,5 @@ mongoose.connect(env.mongoUri)
   })
   .catch(err => {
     console.error('❌ Erro ao conectar ao MongoDB:', err);
->>>>>>> 0e2bd64bc502b4ddaedf59c1161fb53f233dba5b
     process.exit(1);
   });
